29228f02e5a07804716c71c7d42fdf71ff789c38
hamid tehrani <hamid.tehrani2003@gmail.com>
2021-05-05 07:00:46 +0430

    Updates

--- build/middlewares/require-auth.d.ts
+++ build/middlewares/require-auth.d.ts
@@ -1,3 +1,2 @@
 import { Request, Response, NextFunction } from 'express';
-import { NotAuthorizedError } from '../errors/not-authorized-error';
-export declare const requireAuth: (req: Request, res: Response, next: NextFunction) => NotAuthorizedError | undefined;
+export declare const requireAuth: (req: Request, res: Response, next: NextFunction) => void;
--- build/middlewares/require-auth.js
+++ build/middlewares/require-auth.js
@@ -4,7 +4,7 @@ exports.requireAuth = void 0;
 var not_authorized_error_1 = require("../errors/not-authorized-error");
 var requireAuth = function (req, res, next) {
     if (!req.currentUser) {
-        return new not_authorized_error_1.NotAuthorizedError();
+        throw new not_authorized_error_1.NotAuthorizedError();
     }
     next();
 };
